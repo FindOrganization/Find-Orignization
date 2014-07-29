99a8f90db836a75a455942edaeb9496b794c0e77
coolguy101 <jiang_501@hotmail.com>
2014-07-28 19:00:32 -0600

    added some atrr to User and Thread class

--- FindOrignization/src/com/example/model/Thread.java
+++ FindOrignization/src/com/example/model/Thread.java
@@ -1,5 +1,27 @@
 package com.example.model;
 
-public class Thread {
+import java.util.ArrayList;
+import java.util.Date;
 
+import android.graphics.Bitmap;
+
+public class Thread {
+	
+		private String title="";
+		private String content="";
+		private int numberOfViews=0;
+		private Date dateCreated; // need to fetch server date and time
+		private int ID;
+		private Bitmap image;
+		private ArrayList<Bitmap> images;
+		private User author;
+		private int likes=0;
+		private int unlikes=0;
+		private long longitude;
+		private long latitude;
+		// need to finish constructor 
+		public Thread()
+		{
+			
+		}
 }
--- FindOrignization/src/com/example/model/User.java
+++ FindOrignization/src/com/example/model/User.java
@@ -1,7 +1,19 @@
 package com.example.model;
 
 public class User {
-
+	
+	private String userName="";
+	private String email="";
+	private String password="";
+	private String salt="";
+	private int ID;
+	private int phoneNumber;
+	// need to add constructor
+	public User()
+	{
+		
+	}
+	
 
 
 }
